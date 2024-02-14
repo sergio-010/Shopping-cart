abf770a1da41d0ec294b17c831c14820c69e562f
sergio-010 <piconsergio010@gmail.com>
2024-02-13 20:36:46 -0500

    add filter

--- src/pattern/pages/ShoppingPage.tsx
+++ src/pattern/pages/ShoppingPage.tsx
@@ -1,38 +1,38 @@
-import { useContext, useMemo } from "react";
+import { useContext, useState } from "react";
 import { NavBar } from "../components/layouts/NavBar";
 import { useProductos } from "../hooks/useProductos";
 import { ShoppingCartContext } from "../context/ShoppingContext";
 import { FaPlus } from "react-icons/fa";
-import { useSearchParams } from "react-router-dom";
-
-
+import { useFilteredProducts } from "../hooks/useParams";
 
 export const ShoppingPage = () => {
-    const { addToCart } = useContext(ShoppingCartContext)
+    const { addToCart } = useContext(ShoppingCartContext);
     const { productsQuery } = useProductos();
-    const [searchParams] = useSearchParams();
+    const { filterData } = useFilteredProducts();
+    const [searchTerm, setSearchTerm] = useState("");
 
-
-    const filterData = useMemo(() => {
-        const query = searchParams.get('category');
-        console.log(query)
-        if (query) {
-            return productsQuery.data.filter((product) => product.category === query);
-        }
-        return productsQuery.data;
-    }, [productsQuery.data, searchParams])
-
+    const filteredProducts = filterData.filter(product =>
+        product.title?.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
         <>
             <NavBar />
+            <input
+                type="text"
+                placeholder="Buscar..."
+                className="border border-gray-300 rounded"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
             <div className="flex flex-col md:flex-row md:flex-wrap gap-8  mx-auto justify-center">
+
                 {productsQuery.isLoading ? (
                     <p>Loading...</p>
                 ) : productsQuery.isError ? (
                     <p>Error: {productsQuery.error.message}</p>
-                ) : filterData && filterData.length > 0 ? (
-                    filterData.map((product) => (
+                ) : filteredProducts && filteredProducts.length > 0 ? (
+                    filteredProducts.map((product) => (
                         <div className="w-[400px] mx-auto flex flex-col flex-wrap bg-white rounded-lg shadow-md p-4" key={product.id}>
                             <img className="w-full h-[200px] object-contain mb-4 " src={product.image} alt={product.title} />
                             <h2 className="text-lg font-semibold mb-2">{product.title}</h2>
@@ -42,8 +42,6 @@ export const ShoppingPage = () => {
                                     <FaPlus className="mr-1" />
                                     Agregar
                                 </button>
-
-
                             </div>
                         </div>
                     ))
